f68712a15e9dc76d1d6f0f970e78d43723864afe
Asmi0816 <asmi0816@740s-pl-114.local>
2017-03-01 13:49:55 -0700

    All I did was get confused but thats okay because there are three types of teaching. Too easy, Too hard, and hard confusion where you feel you don't learn anything but you stay quiet cuz thats what everyone else does......

--- ArrayProject/Model/CircularList.hpp
+++ ArrayProject/Model/CircularList.hpp
@@ -12,11 +12,31 @@
 #include "DoublyLinckedList.hpp"
 
 template <class Type>
-class CircularList : DoublyLinkedList<Type>
+class CircularList : public DoublyLinkedList<Type>
 {
 private:
-    
+    //no need to redefine private data members.
 public:
+    CircularList();
+    ~CircularList();
+    
+    void add(Type data);
+    Type remove(int index);
+    //other methods.
+    
     
 };
+
+template <class Type>
+CircularList<Type> :: CircularList() : DoublyLinkedList<Type>()
+{
+    //deal with circle list stuff only here.
+    
+}
+
+template <class Type>
+void CircularList<Type> :: add(Type data)
+{
+    
+}
 #endif /* CircularList_h */
