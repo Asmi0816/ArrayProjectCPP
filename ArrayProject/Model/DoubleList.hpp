40b6660c85937923d2758ef5a893202e285fceb5
Asmi0816 <asmi0816@740s-pl-114.local>
2017-03-09 11:51:15 -0700

    Wrote some simple methods for DoubleList.

--- ArrayProject/Model/DoubleList.hpp
+++ ArrayProject/Model/DoubleList.hpp
@@ -9,5 +9,78 @@
 #ifndef DoubleList_h
 #define DoubleList_h
 
+template <classType>
+class DoubleList : public DoublyLinkedList<Type>
+{
+private:
+    
+    
+public:
+    DoubleList();
+    ~DoubleList();
+    void add(Type data);
+    Type remove(int index);
+    void addAtIndexFast(int index, Type value);
+    void addAtIndex(int index, Type value);
+    Type getFromInde(int index);
+    Type getFromIndexFast(int index);
+};
 
+template <class Type>
+void DoubleLis<Type> :: add(Type value)
+{
+    BiDirectionalNode<Type> * addedNode = new BiDirectionalNode<Type>(value);
+    if(this->getSize() == 0)
+    {
+        this->setFront(addedNode);
+    }
+    else
+    {
+        this->getEnd()->setNextPointer(addedNode);
+        addeNode -> setPreviosPointer(this->getEnd());
+    }
+    this->setEnd(addedNode);
+    this->setSize(this->getSize()+1);
+}
+
+
+
+
+template <class Type>
+DoubleList<Type> :: ~DoubleList()
+{
+    BiDirectionalNode<Type> * remove = this->getFront();
+    while(this->getFront() != nullptr)
+    {
+        this->setFront(this->getFront()->getNextPointer());
+        delete remove;
+        remove= this->getFront();
+    }
+}
+
+
+template <class Type>
+Type DoubleLis<Type> :: remove(int index)
+{
+    Type derp;
+    BiDirectionalNode<Type> * nodeToTakeOut = this->getFront();
+    for(int spot = 0; spot < index; spot)
+    {
+        nodeToTakeOut = nodeToTakeOut->getNextPointer();
+    }
+    derp = nodeToTakeOut->getNNextPointer()
+    
+    BiDirectionalNode<Type> * prev = nodeToTakeOut->getPreviousPointer();
+    BiDirectionalNode<Type> * next = nodeToTakeOut->getNextPointer();
+    
+    prev->setNextPointer(next);
+    next->setPrevoiusPointer(prev);
+    
+    delete nodeToTakeOut;
+    
+    this->setSize(this->gettSize()-1);
+    return derp;ß
+    
+    
+}
 #endif /* DoubleList_h */
