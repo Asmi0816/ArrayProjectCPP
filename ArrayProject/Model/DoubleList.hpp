2f6d9e46e19ba7ddb5ed43860d0cd1d20be5d0d2
Asmi0816 <asmi0816@740s-pl-114.local>
2017-03-17 13:18:06 -0600

    Fixed "flub" error.

--- ArrayProject/Model/DoubleList.hpp
+++ ArrayProject/Model/DoubleList.hpp
@@ -78,19 +78,26 @@ Type DoubleList<Type> :: remove(int index)
     derp = nodeToTakeOut->getNodeData();
     if(this->getSize() -1)
     {
-    BiDirectionalNode<Type> * prev = nodeToTakeOut->getPreviousPointer();
-    BiDirectionalNode<Type> * next = nodeToTakeOut->getNextPointer();
+        BiDirectionalNode<Type> * prev = nodeToTakeOut->getPreviousPointer();
+        BiDirectionalNode<Type> * next = nodeToTakeOut->getNextPointer();
     
-    prev->setNextPointer(next);
-    next->setPreviousPointer(prev);
+        if(prev != nullptr)
+        {
+        prev->setNextPointer(next);
+        }
+        if(next != nullptr)
+        {
+        next->setPreviousPointer(prev);
+        }
         if(index == 0)
         {
             this->setFront(this->getFront()->getNextPointer());
-            
+            this->getFront()->setPreviousPointer(nullptr);
         }
         else if(index == this->getSize() -1)
         {
             this->setEnd(this->getEnd()->getPreviousPointer());
+            this->getEnd()->setNextPointer(nullptr);
         }
 
     }
