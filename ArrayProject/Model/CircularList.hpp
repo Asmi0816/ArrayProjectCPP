29d863f4ad2e7e39f96b84d60a3374c33ba4aa34
Asmi0816 <asmi0816@740s-pl-114.local>
2017-03-17 12:02:53 -0600

    added methods to the circular list I missed in class;

--- ArrayProject/Model/CircularList.hpp
+++ ArrayProject/Model/CircularList.hpp
@@ -15,15 +15,14 @@ template <class Type>
 class CircularList : public DoublyLinkedList<Type>
 {
 private:
-    //no need to redefine private data members.
+    BiDirectionalNode<Type> * findNode(int index);
 public:
     CircularList();
     ~CircularList();
-    
     void add(Type data);
     Type remove(int index);
-    //other methods.
-    
+    Type getFromIndex(int index);
+    Type setAtIndex(int index, Type data);
     
 };
 
@@ -33,10 +32,52 @@ CircularList<Type> :: CircularList() : DoublyLinkedList<Type>()
     //deal with circle list stuff only here.
     
 }
+template <class Type>
+CircularList<Type> :: ~CircularList()
+{
+    BiDirectionalNode<Type> * remove = this->getFront();
+    while(this->getFront() != nullptr)
+    {
+        this->setFront(this->getFront()->getNextPointer());
+        delete remove;
+        remove = this->getFront();
+    }
+}
+
+template <class Type>
+BiDirectionalNode<Type> * CircularList<Type> :: findNode(int index)
+{
+    BiDirectionalNode<Type> * nodeToFind;
+    if(index < this->getSize() / 2)
+    {
+        nodeToFind = this->getFrontPointer();
+        for(int spot = 0; spot < index; spot++)
+        {
+            nodeToFind = nodeToFind->getNextPointer();
+        }
+    }
+    else
+    {
+        nodeToFind =this->getEnd();
+        for(int spot = this->getSize() -1; spot > index; spot--)
+        {
+            nodeToFind = nodeToFind->getPreviousPointer();
+        }
+    }
+    return nodeToFind;
+}
+
 
 template <class Type>
 void CircularList<Type> :: add(Type data)
 {
-    
+    BiDirectionalNode<Type> * addMe = new BiDirectionalNode<Type>(data);
+    if(this->getSize() == 0)
+    {
+        this->setFront(addMe);
+        this->setEnd(addMe);
+        addMe->setPreviousPointer(this->getFront());
+        this->getFront()->setNextPointer(this->getEnd());
+    }
 }
 #endif /* CircularList_h */
