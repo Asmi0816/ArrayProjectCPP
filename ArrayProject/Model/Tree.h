733ed5a75acfd303b51b878a25c322bf44890d1f
Asmi0816 <asmi0816@740s-pl-114.local>
2017-04-11 12:34:56 -0600

    Added a new file for a abstract tree.

---
+++ ArrayProject/Model/Tree.h
@@ -0,0 +1,39 @@
+//
+//  Tree.h
+//  ArrayProject
+//
+//  Created by Smith, Arick on 4/11/17.
+//  Copyright © 2017 Smith, Arick. All rights reserved.
+//
+
+#ifndef Tree_h
+#define Tree_h
+
+template <class Type>
+class Tree
+{
+private:
+    int height;
+    int size;
+    bool complete;
+    bool balanced;
+    
+public:
+    Tree();
+    virtual ~Tree();
+    virtual void printToFile() = 0;
+    virtual void inOrderTraversal() = 0;
+    virtual void postOrderTraversal() = 0;
+    virtual void preOrderTraversal() = 0;
+    
+    int getHeight() const;
+    int getSize() const;
+    bool isComplete() const;
+    bool isBalanced() const;
+    
+    void setHeight(int height);
+    void setSize(int size);
+    void setComplete(int complete);
+    void setBalanced(int balanced);
+};
+#endif /* Tree_h */
